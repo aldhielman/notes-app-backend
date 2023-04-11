256b2934a343031c2de72c0093e77bf823756b71
aldhielman <rivaldithoriq@gmail.com>
2023-04-11 13:37:36 +0700

    user registration implemented

--- src/server.js
+++ src/server.js
@@ -2,11 +2,16 @@ require("dotenv").config();
 
 const Hapi = require("@hapi/hapi");
 const notes = require("./api/notes");
+const users = require("./api/users");
 const NotesService = require("./services/postgres/NotesService");
+const UsersService = require("./services/postgres/UsersService");
 const NotesValidator = require("./validator/notes");
+const ClientError = require("./exceptions/ClientError");
+const UsersValidator = require("./validator/users");
 
 const init = async () => {
   const notesService = new NotesService();
+  const usersService = new UsersService();
 
   const server = Hapi.server({
     port: process.env.PORT,
@@ -18,12 +23,51 @@ const init = async () => {
     },
   });
 
-  await server.register({
-    plugin: notes,
-    options: {
-      service: notesService,
-      validator: NotesValidator,
+  await server.register([
+    {
+      plugin: notes,
+      options: {
+        service: notesService,
+        validator: NotesValidator,
+      },
     },
+    {
+      plugin: users,
+      options: {
+        service: usersService,
+        validator: UsersValidator,
+      },
+    },
+  ]);
+
+  server.ext("onPreResponse", (request, h) => {
+    // mendapatkan konteks response dari request
+    const { response } = request;
+    if (response instanceof Error) {
+      // penanganan client error secara internal.
+      if (response instanceof ClientError) {
+        const newResponse = h.response({
+          status: "fail",
+          message: response.message,
+        });
+        newResponse.code(response.statusCode);
+        return newResponse;
+      }
+      // mempertahankan penanganan client error oleh hapi secara native, seperti 404, etc.
+      if (!response.isServer) {
+        return h.continue;
+      }
+      // penanganan server error sesuai kebutuhan
+      const newResponse = h.response({
+        status: "error",
+        message: "terjadi kegagalan pada server kami",
+      });
+      newResponse.code(500);
+      console.log(response);
+      return newResponse;
+    }
+    // jika bukan error, lanjutkan dengan response sebelumnya (tanpa terintervensi)
+    return h.continue;
   });
 
   await server.start();
